dc24249662e2700721cfd033ee10f6307cf066c7
mijinkoo <starmj09@naver.com>
2021-08-17 20:30:35 +0900

    .

---
+++ Level1/210817_짝수와홀수.py
@@ -0,0 +1,10 @@
+# 소스1.
+def solution(num):
+    if num % 2 == 0:
+        return("Even")
+    else:
+        return("Odd")
+
+# 소스2.
+def solution2(num):
+    return "Even" if num % 2 == 0 else "Odd"
